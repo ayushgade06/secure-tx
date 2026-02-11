9cf95eca40f65016bddfe4e60775ac9ba7f3a375
Ayush Gade <ayushgade23@gmail.com>
2026-02-11 23:29:33 +0530

    chore: remove node_modules, dist and turbo cache from repo

--- node_modules/.pnpm/node_modules/@secure-tx/crypto/dist/aes.d.ts
+++
@@ -1,7 +0,0 @@
-export declare function generateRandomBytes(length: number): NonSharedBuffer;
-export declare function aesEncrypt(key: Buffer, plaintext: Buffer): {
-    nonce: NonSharedBuffer;
-    ciphertext: Buffer<ArrayBuffer>;
-    tag: NonSharedBuffer;
-};
-export declare function aesDecrypt(key: Buffer, nonce: Buffer, ciphertext: Buffer, tag: Buffer): Buffer<ArrayBuffer>;
